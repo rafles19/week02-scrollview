30cf6558e5c41f2b19730dcff59737aae371d07d
rafleskristiyanto <sitiora19@gmail.com>
2024-09-10 22:56:01 +0700

    Week02

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, ScrollView, View, Text } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -7,64 +7,37 @@ import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ScrollView>
+            <View>
+            <Profile nim= "8000000222" nama= "John Smith" image = "https://randomuser.me/api/portraits/men/75.jpg"/>
+            <Profile nim= "8000000222" nama= "Jane Doe" image = "https://randomuser.me/api/portraits/women/75.jpg"/>
+            <Profile nim= "8000000222" nama= "Michael Johnson" image = "https://randomuser.me/api/portraits/men/76.jpg"/>
+            <Profile nim= "8000000222" nama= "Emily Williams" image = "https://randomuser.me/api/portraits/men/77.jpg"/>
+            <Profile nim= "8000000222" nama= "William Brown" image = "https://randomuser.me/api/portraits/women/77.jpg"/>
+            <Profile nim= "8000000222" nama= "Olivia Jones" image = "https://randomuser.me/api/portraits/men/78.jpg"/>
+            <Profile nim= "8000000222" nama= "James Miller" image = "https://randomuser.me/api/portraits/women/78.jpg"/>
+            <Profile nim= "8000000222" nama= "Sophia Davis" image = "https://randomuser.me/api/portraits/men/79.jpg"/>
+            <Profile nim= "8000000222" nama= "Ethan Garcia" image = "https://randomuser.me/api/portraits/women/79.jpg"/>
+            <Profile nim= "8000000222" nama= "Ava Rodriguez" image = "https://randomuser.me/api/portraits/men/80.jpg"/>
+            <Profile nim= "8000000222" nama= "David Wilson" image = "https://randomuser.me/api/portraits/women/80.jpg"/>
+        </View>
+        </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+
+const Profile = props => {
+  return (
+      <View style = {{ alignItems: 'center', margin : 50}}>
+      <Image
+          source={{
+          uri: props.image,
+          }}
+          style={{ width: 100, height: 100, margin: 10 }} />
+      <Text>{props.nama}</Text>
+      <Text>{props.nim}</Text>
+      </View>
+  )
+  } 
+
+
